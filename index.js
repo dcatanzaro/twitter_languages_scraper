c373c8127cd0a80ca55fb03a72b2afe607689ea5
Damián <damiancatanzaro@gmail.com>
2019-10-06 17:57:09 -0300

    add split by ',' and '/'

--- index.js
+++ index.js
@@ -35,34 +35,44 @@ function getAllTweets(maxId = "") {
                     let split = tw ? tw.split(":")[1] : "";
 
                     if (split) {
+                        split = split.split(/,|\//);
+
                         if (tw.includes(["first language"])) {
-                            tweetSanitized[0] = split.trim();
+                            tweetSanitized[0] = split;
                         } else if (tw.includes(["had difficulties"])) {
-                            tweetSanitized[1] = split.trim();
+                            tweetSanitized[1] = split;
                         } else if (tw.includes(["most used"])) {
-                            tweetSanitized[2] = split.trim();
+                            tweetSanitized[2] = split;
                         } else if (tw.includes(["totally hate"])) {
-                            tweetSanitized[3] = split.trim();
+                            tweetSanitized[3] = split;
                         } else if (tw.includes(["most loved"])) {
-                            tweetSanitized[4] = split.trim();
+                            tweetSanitized[4] = split;
                         } else if (tw.includes(["for beginners"])) {
-                            tweetSanitized[5] = split.trim();
+                            tweetSanitized[5] = split;
                         }
                     }
                 });
 
                 tweetSanitized.map((language, index) => {
-                    const indexLanguage = arLanguages.indexOf(language);
-                    const sanitizedLanguage =
-                        indexLanguage > -1 ? arLanguages[indexLanguage] : null;
+                    language.map(tmpLng => {
+                        tmpLng = tmpLng.trim();
 
-                    if (sanitizedLanguage && index < 6) {
-                        if (typeof arResults[index][language] !== "undefined") {
-                            arResults[index][language]++;
-                        } else {
-                            arResults[index][language] = 1;
+                        const indexLanguage = arLanguages.indexOf(tmpLng);
+                        const sanitizedLanguage =
+                            indexLanguage > -1
+                                ? arLanguages[indexLanguage]
+                                : null;
+
+                        if (sanitizedLanguage && index < 6) {
+                            if (
+                                typeof arResults[index][tmpLng] !== "undefined"
+                            ) {
+                                arResults[index][tmpLng]++;
+                            } else {
+                                arResults[index][tmpLng] = 1;
+                            }
                         }
-                    }
+                    });
                 });
 
                 lastId = tweet.id;
@@ -82,15 +92,15 @@ function getAllTweets(maxId = "") {
 
 getAllTweets();
 
-// const results = arResults.map(result => {
-//     return orderObj(result);
-// });
-// console.log("First language", results[0]);
-// console.log("Had difficulties", results[1]);
-// console.log("Most used", results[2]);
-// console.log("Totally hate", results[3]);
-// console.log("Most loved", results[4]);
-// console.log("For beginners", results[5]);
+const results = arResults.map(result => {
+    return orderObj(result);
+});
+console.log("First language", results[0]);
+console.log("Had difficulties", results[1]);
+console.log("Most used", results[2]);
+console.log("Totally hate", results[3]);
+console.log("Most loved", results[4]);
+console.log("For beginners", results[5]);
 
 function orderObj(obj) {
     const keys = Object.keys(obj).sort((a, b) => (obj[a] < obj[b] ? 1 : -1));
